f25763b81c107c13bbfc138d352c6b4560345782
Evan Harmony <evan_harmony@hotmail.com>
2024-12-03 23:16:30 -0500

    Adding test data and structure of data for easier typing throughout test

---
+++ data/project-board-type.ts
@@ -0,0 +1,6 @@
+export interface TestData {
+    project: 'Web Application' | 'Mobile Application' | 'Marketing Campaign';
+    ticket: string;
+    column: 'To Do' | 'In Progress' | 'Review' | 'Done';
+    tags: string[];
+}
