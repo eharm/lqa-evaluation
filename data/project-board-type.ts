2a8499208c97b3299f57bd4573db7cd1a0c97864
Evan Harmony <evan_harmony@hotmail.com>
2024-12-04 00:54:11 -0500

    Updating TestData to be a type instead of an interface

--- data/project-board-type.ts
+++ data/project-board-type.ts
@@ -1,4 +1,4 @@
-export interface TestData {
+export type TestData = {
     project: 'Web Application' | 'Mobile Application' | 'Marketing Campaign';
     ticket: string;
     column: 'To Do' | 'In Progress' | 'Review' | 'Done';
